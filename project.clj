670976587f7a55393eb6a9f1e15f93cba8338c34
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2016-07-06 23:37:28 +0900

    remove unnecessary dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [proto-repl "0.1.2"]
-                 [proto-repl-charts "0.2.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :profiles
   {:dev {:source-paths ["src" "test"]
-         :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
+         :dependencies []}})
